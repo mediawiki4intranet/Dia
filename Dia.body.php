38f45ef4623c1bec047e7d0a14b02589d6ca11ae
Vitaliy Filippov <vitalif@yourcmc.ru>
2011-07-26 14:19:28 +0000

    Bug 63356 - Dia diagram support

---
+++ Dia.body.php
@@ -0,0 +1,165 @@
+<?php
+
+function wfGetDIAsize($filename)
+{
+    global $wgDIANominalSize;
+
+    $xmlstr = file_get_contents($filename);
+    $xmlstr = str_replace("<dia:", "<", $xmlstr);
+    $xmlstr = str_replace("</dia:", "</", $xmlstr);
+
+    $xml = new SimpleXMLElement($xmlstr);
+
+    $xmin = $ymin = $xmax = $ymax = -1;
+
+    /*
+     * Get the total bounding box (for correct aspect ratio).
+     *
+     * Note: the loop expects $x1<=$x2 and $y1<=$y2, but dia does this for you.
+     */
+    foreach($xml->xpath('//attribute[@name="obj_bb"]') as $boundingbox)
+    {
+        sscanf($boundingbox->rectangle['val'], "%f,%f;%f,%f", $x1, $y1, $x2, $y2);
+
+        if ($xmin == -1 || $x1 < $xmin)
+            $xmin = $x1;
+        if ($xmax == -1 || $x2 > $xmax)
+            $xmax = $x2;
+
+        if ($ymin == -1 || $y1 < $ymin)
+            $ymin = $y1;
+        if ($ymax == -1 || $y2 > $ymax)
+            $ymax = $y2;
+    }
+
+    $width = $xmax - $xmin;
+    $height = $ymax - $ymin;
+
+    $aspect = $width / $height;
+
+    $nominal_width = $wgDIANominalSize;
+    $nominal_height = (int)($nominal_width / $aspect);
+
+    $res = array($nominal_width, $nominal_height);
+
+    return $res;
+}
+
+/**
+ * @addtogroup Media
+ */
+class DiaHandler extends ImageHandler
+{
+    function isEnabled()
+    {
+        global $wgDIAConverters, $wgDIAConverter;
+        if (!isset($wgDIAConverters[$wgDIAConverter]))
+        {
+            wfDebug("\$wgDIAConverter is invalid, disabling DIA rendering.\n");
+            return false;
+        }
+        return true;
+    }
+
+    function mustRender($file)
+    {
+        return true;
+    }
+
+    function canRender($file)
+    {
+        return true;
+    }
+
+    function normaliseParams($image, &$params)
+    {
+        global $wgDIAMaxSize;
+        if (!parent::normaliseParams($image, $params))
+            return false;
+
+        // Don't make an image bigger than wgMaxDIASize
+        $params['physicalWidth'] = $params['width'];
+        $params['physicalHeight'] = $params['height'];
+        if ($params['physicalWidth'] > $wgDIAMaxSize)
+        {
+            $srcWidth = $image->getWidth($params['page']);
+            $srcHeight = $image->getHeight($params['page']);
+            $params['physicalWidth'] = $wgDIAMaxSize;
+            $params['physicalHeight'] = File::scaleHeight($srcWidth, $srcHeight, $wgDIAMaxSize);
+        }
+        return true;
+    }
+
+    function doTransform($image, $dstPath, $dstUrl, $params, $flags = 0)
+    {
+        global $wgDIAConverters, $wgDIAConverter, $wgDIAConverterPath;
+
+        if (!$this->normaliseParams($image, $params))
+            return new TransformParameterError($params);
+
+        $clientWidth = $params['width'];
+        $clientHeight = $params['height'];
+        $physicalWidth = $params['physicalWidth'];
+        $physicalHeight = $params['physicalHeight'];
+        $srcPath = $image->getPath();
+
+        if ($flags & self::TRANSFORM_LATER)
+            return new ThumbnailImage($image, $dstUrl, $clientWidth, $clientHeight, $dstPath);
+
+        if (!wfMkdirParents(dirname($dstPath)))
+        {
+            return new MediaTransformError(
+                'thumbnail_error', $clientWidth, $clientHeight,
+                wfMsg('thumbnail_dest_directory')
+            );
+        }
+
+        $err = false;
+        if (isset($wgDIAConverters[$wgDIAConverter]))
+        {
+            $cmd = str_replace(
+                array('$path/', '$width', '$height', '$input', '$output'),
+                array($wgDIAConverterPath ? wfEscapeShellArg("$wgDIAConverterPath/") : "",
+                       intval($physicalWidth),
+                       intval($physicalHeight),
+                       wfEscapeShellArg($srcPath),
+                       wfEscapeShellArg($dstPath)),
+                $wgDIAConverters[$wgDIAConverter]) . " 2>&1";
+            wfProfileIn('dia');
+            wfDebug(__METHOD__.": $cmd\n");
+            $err = wfShellExec($cmd, $retval);
+            wfProfileOut('dia');
+        }
+
+        $removed = $this->removeBadFile($dstPath, $retval);
+        if ($retval != 0 || $removed)
+        {
+            wfDebugLog('thumbnail',
+                sprintf('thumbnail failed on %s: error %d "%s" from "%s"',
+                    wfHostname(), $retval, trim($err), $cmd));
+            return new MediaTransformError('thumbnail_error', $clientWidth, $clientHeight, $err);
+        }
+        else
+            return new ThumbnailImage($image, $dstUrl, $clientWidth, $clientHeight, $dstPath);
+    }
+
+    function getImageSize($image, $path)
+    {
+        return wfGetDIAsize($path);
+    }
+
+    function getThumbType($ext, $mime)
+    {
+        return array('png', 'image/png');
+    }
+
+    function getLongDesc($file)
+    {
+        global $wgLang;
+        wfLoadExtensionMessages('Dia');
+        return wfMsg(
+            'dia-long-desc', $file->getWidth(), $file->getHeight(),
+            $wgLang->formatSize($file->getSize())
+        );
+    }
+}
---
+++ Dia.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Setup for Dia extension, an extension that allows Dia (http://live.gnome.org/Dia) diagrams
+ * to be rendered in MediaWiki pages.
+ * Fixed for 1.16 + reformatted by Vitaliy Filippov, http://wiki.4intra.net/
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Marcel Toele, dwarfhouse.org
+ * @copyright © 2007 Marcel Toele
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+if (!defined('MEDIAWIKI'))
+{
+    echo("This file is an extension to the MediaWiki software and cannot be used standalone.\n");
+    die(1);
+}
+
+// Credits
+$wgExtensionCredits['other'][] = array(
+    'name'        => 'Dia',
+    'author'      => 'Marcel Toele',
+    'url'         => 'http://mediawiki.org/wiki/Extension:Dia',
+    'description' => 'Allows Dia diagrams to be rendered inside MediaWiki pages.',
+);
+
+// Default Config
+
+// Dia diagrams may be uploaded as drawings.
+// Dia diagrams are converted to png before they can be rendered on a page.
+// Future versions of the extension may also output SVG.
+//
+// An external program is required to perform this conversion:
+$wgDIAConverters = array(
+    // dia -n -e testdiagram.png -t png -s 100 testdiagram.dia
+    'dia' => '$path/dia -n -e $output -t png -s $width $input',
+);
+// Pick one of the above
+$wgDIAConverter = 'dia';
+// If not in the executable PATH, specify
+$wgDIAConverterPath = '';
+// The nominal width of a Dia file when rendered to png
+$wgDIANominalSize = 300;
+// Don't scale a Dia file larger than this
+$wgDIAMaxSize = 1024;
+
+// Add the DiaHandler via the Autoload mechanism.
+$wgMediaHandlers['application/x-dia-diagram'] = 'DiaHandler';
+$wgAutoloadClasses['DiaHandler'] = dirname(__FILE__) . '/Dia.body.php';
+$wgExtensionMessagesFiles['Dia'] = dirname(__FILE__) . '/Dia.i18n.php';
